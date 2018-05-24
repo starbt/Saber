ed7ae1b6c46bd627775910fba53141d85d76beca
starbt <starbter@gmail.com>
2018-05-25 01:10:00 +0800

    double buffer for shared_ptr

--- src/manage/config_loader.cc
+++ src/manage/config_loader.cc
@@ -5,6 +5,8 @@
 
 ConfigLoader* ConfigLoader::config_loader_ptr_ = NULL;
 
+std::mutex ConfigLoader::mutex_;
+
 bool ConfigLoader::Load(const std::string& conf_path)
 {
     std::ifstream fin(conf_path);  
@@ -18,11 +20,11 @@ bool ConfigLoader::Load(const std::string& conf_path)
     {  
         str_file = str_file + temp + std::string("\n");  
     }  
+
     //parse to json 
-    config_document_ptr_ = std::make_shared<rapidjson::Document>();
-    config_document_ptr_->Parse<0>(str_file.c_str());  
-    //rapidjson::Value &node1 = (*config_document_ptr_)["author"];  
-    //std::cout << "author: " << node1.GetString() << std::endl;  
+    auto config_ptr = std::make_shared<rapidjson::Document>();
+    config_ptr->Parse<0>(str_file.c_str());  
+    UpdateConfigPtr(config_ptr);
 
     return true;
 }
--- src/manage/config_loader.h
+++ src/manage/config_loader.h
@@ -10,32 +10,54 @@
 #include <fstream>
 #include <memory>
 #include <string>
+#include <mutex>
+#include <atomic>
 
 class ConfigLoader
 {
 public:
     static ConfigLoader* GetInstance()
-    {
-        if (config_loader_ptr_ == NULL) {
-            config_loader_ptr_ = new ConfigLoader();
-        }
-        return config_loader_ptr_;
+    { 
+       if (config_loader_ptr_ == NULL) {
+            std::lock_guard<std::mutex> lock(mutex_);
+            if (config_loader_ptr_ == NULL) {
+                config_loader_ptr_ = new ConfigLoader();
+            }
+       }
+       return config_loader_ptr_;
     }
 
     bool Load(const std::string& conf_path);
-    
-    std::shared_ptr<rapidjson::Document> config_document_ptr()
-    { return config_document_ptr_; }
 
+    //get the config ptr 
+    std::shared_ptr<rapidjson::Document> config_document_ptr()
+    { 
+        auto index = cur_index_.load(std::memory_order_relaxed);
+        return config_document_ptr_[index];
+    }
+
+    void UpdateConfigPtr(std::shared_ptr<rapidjson::Document>  ptr)
+    {
+        int cur_index = cur_index_.load(std::memory_order_relaxed);
+        int next_index = (cur_index + 1) % 2;
+        config_document_ptr_[next_index] = ptr;
+        cur_index_.store(next_index, std::memory_order_relaxed);
+    }
 
 private:
-    ConfigLoader()
-     : config_document_ptr_(NULL)
-     { }
+    ConfigLoader() 
+     : cur_index_(0)
+    {
+        config_document_ptr_[0] = NULL;
+        config_document_ptr_[1] = NULL;
+    } 
     virtual ~ConfigLoader() = default;
+
 private:
     static ConfigLoader* config_loader_ptr_;
-    std::shared_ptr<rapidjson::Document> config_document_ptr_;
+    std::shared_ptr<rapidjson::Document> config_document_ptr_[2];
+    std::atomic<int> cur_index_;
+    static std::mutex mutex_;
 };
 
 #endif
